0b4f7ad108bb7a72345cce6a18b836ec810552a2
Wojciech Kaczmarek <wojtekk@kofeina.net>
2018-11-13 12:58:12 +0100

    basic processing: try to run few well known progs

--- process.go
+++ process.go
@@ -1,10 +1,39 @@
 package main
 
 import (
-// "os/exec" TODO
+	"os"
+	"os/exec"
 )
 
+var progs = []string{
+	"./.forever.step",
+	"make",
+}
+
 func process() {
-	println("WIP; not running anything yet!")
-	//...
+	var done bool
+	for _, p := range progs {
+		if done {
+			break
+		}
+		c := exec.Command(p)
+		c.Stdout = os.Stdout
+		c.Stderr = os.Stderr
+		err := c.Run()
+		if progNotFound(err) {
+			continue
+		}
+		if err != nil {
+			log(err)
+			continue
+		}
+		done = true
+	}
+	if !done {
+		log("could not find suitable program to run")
+	}
+}
+
+func progNotFound(err error) bool {
+	return os.IsNotExist(err) || err == exec.ErrNotFound
 }
